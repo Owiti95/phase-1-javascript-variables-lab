9f386e4c499359b72de03ad195e9d5d3e08df137
hannington owiti <hanningtonowiti@gmail.com>
2024-06-26 11:46:57 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution in this file!
+//Code your solution in this file!
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+//it('is set as Chelsea', function () {
+//    expect(mostProfitableNeighborhood).to.equal('Chelsea');
+//});
+
+let companyCeo = 'Susan Smith';
+
+
+
+//it('is set as SusanSmith', function () {
+//    expect(companyCeo).to.equal('SusanSmith');
+//});
